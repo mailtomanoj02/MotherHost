82d33c6185fba90ddd5d271726154d49d38c4499
fh-manoj <88829037+fh-manoj@users.noreply.github.com>
2023-01-22 16:58:58 +0530

    Home Screen navigation and Api Changes

--- motherhost/src/Components/Toast.js
+++ motherhost/src/Components/Toast.js
@@ -1,13 +1,103 @@
-import {Text, View} from 'react-native';
-import Toast from 'react-native-toast-message';
-import React from '.';
+import React, {useEffect, useRef, useState} from 'react';
 
-const ToastMessage = () => {
+import {Animated, Button, Text, View} from 'react-native';
+import {FONT_FAMILY} from '../Config/Constant';
+
+const Message = props => {
+  const opacity = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.sequence([
+      Animated.timing(opacity, {
+        toValue: 1,
+        duration: 500,
+        useNativeDriver: true,
+      }),
+      Animated.delay(2000),
+      Animated.timing(opacity, {
+        toValue: 0,
+        duration: 500,
+        useNativeDriver: true,
+      }),
+    ]).start(() => {
+      props.onHide();
+    });
+  }, []);
+
   return (
-    <View>
-        <Toast/>
-    </View>
-
+    <Animated.View
+      style={{
+        opacity,
+        transform: [
+          {
+            translateY: opacity.interpolate({
+              inputRange: [0, 1],
+              outputRange: [-20, 0],
+            }),
+          },
+        ],
+        margin: 10,
+        marginBottom: 5,
+        backgroundColor: props.color,
+        padding: 10,
+        borderRadius: 4,
+        shadowColor: 'black',
+        shadowOffset: {
+          width: 0,
+          height: 3,
+        },
+        shadowOpacity: 0.15,
+        shadowRadius: 5,
+        elevation: 6,
+      }}>
+      <Text style={{color: 'white', fontFamily: FONT_FAMILY.SEMI_BOLD}}>
+        {props.message}
+      </Text>
+    </Animated.View>
   );
 };
-export default ToastMessage;
+
+const Toast = props => {
+  const [messages, setMessages] = useState([]);
+  console.log(props.backgroundColor);
+  const color = props.backgroundColor;
+  useEffect(() => {
+    const message = props.message;
+    setMessages([...messages, message]);
+  }, []);
+
+  return (
+    <>
+      <View
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+        }}>
+        {messages.map(message => (
+          <Message
+            key={message}
+            message={message}
+            color={color}
+            onHide={() => {
+              setMessages(messages =>
+                messages.filter(currentMessage => currentMessage !== message),
+              );
+            }}
+          />
+        ))}
+      </View>
+
+      {/*<Button*/}
+      {/*  title="Add message"*/}
+      {/*  onPress={() => {*/}
+      {/*    const message = props.message;*/}
+      {/*    setMessages([...messages, message]);*/}
+      {/*  }}*/}
+      {/*/>*/}
+    </>
+  );
+};
+
+export default Toast;
--- motherhost/src/Components/loginAndRegistration/SignIn.js
+++ motherhost/src/Components/loginAndRegistration/SignIn.js
@@ -10,13 +10,24 @@ import Colors from '../../Themes/Colors';
 import {FONT_FAMILY} from '../../Config/Constant';
 import colors from '../../Themes/Colors';
 import PlaceHolderComponent from './PlaceHolderComponent';
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import SubmitButton from './SubmitButton';
+import Toast from '../Toast';
 const SignIn = () => {
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
+  const [toast, setToast] = useState(false);
+  const onPress = () => {
+    setToast(true);
+    setTimeout(() => {
+      setToast(false);
+    }, 2000);
+  };
   return (
     <View style={styles.containerStyle} onTouchStart={() => Keyboard.dismiss()}>
+      {toast ? (
+        <Toast message={'Provide all information'} backgroundColor={'red'} />
+      ) : null}
       <Text style={styles.signInTextStyle}>SIGN IN</Text>
       <PlaceHolderComponent
         image={require('../../Images/EntryIcons/user.png')}
@@ -38,7 +49,12 @@ const SignIn = () => {
           placeholder: 'Password',
         }}
       />
-      <SubmitButton title={'SIGN IN'} />
+      <TouchableOpacity
+        style={styles.buttonContainer}
+        onPress={() => onPress()}>
+        <Text style={styles.buttonTextStyle}>SUBMIT</Text>
+        {/*<Text>SUBMIT</Text>*/}
+      </TouchableOpacity>
       <View style={{flexDirection: 'row', marginTop: 20}}>
         <Text
           style={{
@@ -70,5 +86,19 @@ const styles = StyleSheet.create({
     color: colors.headerBlue,
     fontFamily: FONT_FAMILY.SEMI_BOLD,
     fontSize: 16,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    backgroundColor: Colors.ORANGE,
+    marginTop: 20,
+    padding: 10,
+    marginHorizontal: 12,
+  },
+  buttonTextStyle: {
+    flex: 1,
+    textAlign: 'center',
+    color: Colors.white,
+    fontSize: 17,
+    fontFamily: FONT_FAMILY.SEMI_BOLD,
   },
 });
